0005c7ef305a925d33a2c502bd24af30f99c5059
Adequm <adequm@bk.ru>
2022-03-12 17:24:50 +0300

    init

---
+++ precache-manifest.fa41751b1f8b9c6c8cf50f633a250f29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8eb461a63ae213a170",
+    "url": "/minis-docx-template/css/app.5d42b35a.css"
+  },
+  {
+    "revision": "a18187d9cbbd54fdbbca3208dec7ac6b",
+    "url": "/minis-docx-template/icon.svg"
+  },
+  {
+    "revision": "14a9dfc6e87ab2fef46516e4f8aa058e",
+    "url": "/minis-docx-template/img/confetti.14a9dfc6.svg"
+  },
+  {
+    "revision": "2c328a24349d2d974d357a1dc248dfbf",
+    "url": "/minis-docx-template/index.html"
+  },
+  {
+    "revision": "3b8eb461a63ae213a170",
+    "url": "/minis-docx-template/js/app.0b630c89.js"
+  },
+  {
+    "revision": "61f0d8e142641d17d592",
+    "url": "/minis-docx-template/js/chunk-vendors.a59a423a.js"
+  },
+  {
+    "revision": "8ebbeb0f779992263edc871faf0724fb",
+    "url": "/minis-docx-template/manifest.json"
+  }
+]);
