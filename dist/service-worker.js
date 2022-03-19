1625a4a82c3ff1c582f953cd7590de9d2afced1d
Adequm <adequm@bk.ru>
2022-03-19 18:10:32 +0300

    dist payload

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minis-docx-template/precache-manifest.613d88b572b180848f82cc42bc46eea3.js"
+  "/minis-docx-template/precache-manifest.e876c2fda981acaa711badf3523d007d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "docx-template"});
